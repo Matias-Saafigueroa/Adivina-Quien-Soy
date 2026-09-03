d9da99d7dcdc8b834759839e3d398f301bfb1f55
Cruzlobo05 <clobo@uade.edu.ar>
2026-09-03 12:53:28 -0300

    Cambio de menu para jugar contra las 2 maquinas y arreglo de bugs de consola

---
+++ src/main/java/Juego/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import Servicios.Juego;
+
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class Main {
+
+    private static final String ARCHIVO_PERSONAJES = "src/Data/personajes.txt";
+    private static final String ARCHIVO_MARCADOR = "src/Data/marcador.txt";
+
+    public static void main(String[] args) {
+        System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
+        Scanner teclado = new Scanner(System.in, StandardCharsets.UTF_8);
+
+        Juego juego = new Juego(ARCHIVO_PERSONAJES, ARCHIVO_MARCADOR, teclado);
+        new MenuPrincipal(juego, teclado).iniciar();
+    }
+}
